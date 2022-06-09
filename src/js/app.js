b9d87f66964b1736666de31e301f068bf2e7228c
tk1128kami <tk1128kami@icloud.com>
2022-06-09 16:00:13 +0900

    setting react

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,12 @@
 import bar from './bar.js';
 
 bar();
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const APP = () => {
+  return <h1>Hello React</h1>;
+};
+
+ReactDOM.render(<APP />, document.getElementById("app"));
